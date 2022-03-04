542c30add2960d620045be2f6f1fd821a06f5123
biit-hub <beat.sturzi@hotmail.ch>
2022-03-04 20:51:29 +0100

    updated Documentation

--- doxygen/html/search/all_c.js
+++ doxygen/html/search/all_c.js
@@ -6,5 +6,5 @@ var searchData=
   ['pages_5farray_3',['pages_array',['../main_8cpp.html#a84ad797bb04f41d1c79aa573addc579e',1,'pages_array():&#160;pages.c'],['../pages_8c.html#a84ad797bb04f41d1c79aa573addc579e',1,'pages_array():&#160;pages.c']]],
   ['password_4',['password',['../secrets_8h.html#aa4a2ebcb494493f648ae1e6975672575',1,'password():&#160;secrets.h'],['../wio__wifi_8h.html#aa4a2ebcb494493f648ae1e6975672575',1,'password():&#160;secrets.h']]],
   ['periodictasks_5',['periodicTasks',['../main_8cpp.html#ace6f96c5f8bfcd98de2277be4b429de0',1,'main.cpp']]],
-  ['publishtopic_6',['publishTopic',['../classwio__mqtt.html#aeee8c47685fa66cf48de30ee1b7f45e4',1,'wio_mqtt']]]
+  ['publishtopic_6',['publishTopic',['../classwio__mqtt.html#a4515c2118e8e2a8d7298a9e9c2154710',1,'wio_mqtt::publishTopic(const char *topic, char *payload, bool retain)'],['../classwio__mqtt.html#a37c60310e7f849314eb9c785fb1d664b',1,'wio_mqtt::publishTopic(const char *topic, const char *payload, bool retain)']]]
 ];
--- doxygen/html/search/functions_8.js
+++ doxygen/html/search/functions_8.js
@@ -1,5 +1,5 @@
 var searchData=
 [
   ['periodictasks_0',['periodicTasks',['../main_8cpp.html#ace6f96c5f8bfcd98de2277be4b429de0',1,'main.cpp']]],
-  ['publishtopic_1',['publishTopic',['../classwio__mqtt.html#aeee8c47685fa66cf48de30ee1b7f45e4',1,'wio_mqtt']]]
+  ['publishtopic_1',['publishTopic',['../classwio__mqtt.html#a4515c2118e8e2a8d7298a9e9c2154710',1,'wio_mqtt::publishTopic(const char *topic, char *payload, bool retain)'],['../classwio__mqtt.html#a37c60310e7f849314eb9c785fb1d664b',1,'wio_mqtt::publishTopic(const char *topic, const char *payload, bool retain)']]]
 ];
